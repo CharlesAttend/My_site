c5347774fdcad2505bb0368ee860feeae90efb75
Charles <charlo22@hotmail.fr>
2022-01-12 12:43:44 +0100

    commit to sync

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,19 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    colors: {
+      'cwhite': '#F4F9E9',
+      'cgray': '#EEF0EB',
+      'cpurple': '#5B2A86',
+      'cdarkblue': '#360568',
+      'clightblue': '#7785AC',
+      'cblack': '#1b0234',
+    },
+    fontFamily: {
+      'body': ['"Open Sans"'],
+    },
     extend: {},
   },
   plugins: [],
 }
+// https://coolors.co/360568-5b2a86-7785ac-f4f9e9-eef0eb
