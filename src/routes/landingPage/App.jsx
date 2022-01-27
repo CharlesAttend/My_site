41a85ba8877cafb853441b96a94bb25dd1bf26c8
Charles <charlo22@hotmail.fr>
2022-01-27 19:58:58 +0100

    fixed height of social link

--- src/routes/landingPage/App.jsx
+++ src/routes/landingPage/App.jsx
@@ -127,9 +127,9 @@ const Projet = () => (
 )
 
 const SocialLink = () => {
-  return (<section className="h-screen bg-white">
+  return (<section className="h-screen lg:h-96 bg-white">
     <div className="flex flex-col items-center h-full justify-evenly ">
-      <span className="text-cdpurple text-4xl">Contact</span>
+      <span className="text-cdpurple text-4xl font-semibold">Contact</span>
       <div className="w-10/12 lg:no-flex grid gap-5 grid-row-4 sm:gap-10 lg:grid-row-none lg:grid-cols-4  xl:gap-32 " >
           <div className="flex items-center justify-between  lg:flex-col-reverse lg:bg-gray-50 lg:p-6 lg:h-full">
             <div className="lg:flex flex-col-reverse items-center">
