ef6a942e17c614df6bb4f061466f46bc5e0304b9
Charles Attend <charles.vin@outlook.fr>
2022-01-27 12:44:19 +0100

    Carousel + commit to sync

--- src/routes/landingPage/CarouselComponent.jsx
+++ src/routes/landingPage/CarouselComponent.jsx
@@ -1,49 +1,34 @@
-import 'react-multi-carousel/lib/styles.css';
-import Carousel from 'react-multi-carousel';
+import Carousel from 'react-elastic-carousel';
 import { ReactComponent as CloudLogo } from './img/cloud-download-alt-solid.svg'
 import monte_carlo_pi from './img/monte-carlo-pi.png'
 
 const CarouselComponent = () => {
-	const responsive = {
-		superLargeDesktop: {
-			// the naming can be any, depends on you.
-			breakpoint: { max: 4000, min: 3000 },
-			items: 5
-		},
-		desktop: {
-			breakpoint: { max: 3000, min: 1024 },
-			items: 3
-		},
-		tablet: {
-			breakpoint: { max: 1024, min: 464 },
-			items: 2
-		},
-		mobile: {
-			breakpoint: { max: 464, min: 0 },
-			items: 1
-		}
-	};
 
+	const breakpoint = [
+        { width: 640, itemsToShow: 1 },
+        { width: 768, itemsToShow: 2, itemsToScroll: 2, pagination: false },
+        { width: 1024, itemsToShow: 3 },
+	];
 	return (
-		<Carousel className='w-screen text-cgray' responsive={responsive}>
+		<Carousel className='text-cgray' breakPoints={breakpoint} itemPadding={[10]}>
 
-			<div className="bg-white p-3 rounded-lg flex flex-col items-center">
+			<div className="bg-white flex-grow p-3 rounded-lg flex flex-col items-center">
 				<CloudLogo className="text-cdpurple w-2/4 pb-2" />
-				<div className="text-cdpurple text-2xl">MusicSync</div>
-				<div className="">
-					<h2>Téléchargement et synchronisation de playlist SoundCloud et YouTube.</h2>
+				<div className="text-cdpurple text-2xl text-center">MusicSync</div>
+				<div className="flex flex-col items-center">
+					<h2 className="text-center">Téléchargement et synchronisation de playlist SoundCloud et YouTube.</h2>
 					<div className="flex flex-wrap justify-center mb-3">
-						{['python', 'crontab', 'owncloud', 'webdav', 'youtubeDL'].map(
+						{['Python', 'Crontab', 'Owncloud', 'Webdav', 'YoutubeDL'].map(
 							(keyword, index) => <div className="border rounded-lg m-1 p-1 whitespace-nowrap" key={index}>{keyword}</div>)}
 					</div>
 					<p><a href="https://django.charles.vin/musicsync" class="border border-cpurple p-2 m-2 rounded-md hover:bg-cpurple hover:text-white">Accéder à l'interface</a></p>
 				</div>
 			</div>
 
-			<div className="bg-white p-3 rounded-lg flex flex-col items-center">
+			<div className="bg-white flex-grow p-3 rounded-lg flex flex-col items-center">
 				<img src={monte_carlo_pi} alt="the web project" className="pb-2" />
-				<div className="text-cdpurple text-2xl">Visualisation de la méthode Monte-Carlos pour estimer Pi</div>
-				<div className="">
+				<div className="text-cdpurple text-2xl text-center">Visualisation de la méthode Monte-Carlos pour estimer Pi</div>
+				<div className="flex flex-col items-center">
 					<div className="flex flex-wrap justify-center mb-3">
 						{['JavaScript', 'Html', 'Css', 'Mathématiques'].map(
 							(keyword, index) => <div className="border rounded-lg m-1 p-1 whitespace-nowrap" key={index}>{keyword}</div>)}
@@ -52,11 +37,11 @@ const CarouselComponent = () => {
 				</div>
 			</div>
 
-			<div className="bg-white p-3 rounded-lg flex flex-col items-center">
+			<div className="bg-white flex-grow p-3 rounded-lg flex flex-col items-center">
 				<img src={monte_carlo_pi} alt="image of the web project" className="pb-2" />
-				<div className="text-cdpurple text-2xl">Recipe Chooser</div>
-				<div className="">
-					<h2>Proposition de recettes à partir d'une liste d'ingrédients disponibles. Et autocomplétion des ingrédients durant la saisie. J'aimerai beaucoup pousser ce projet dans le futur !</h2>
+				<div className="text-cdpurple text-2xl text-center">Recipe Chooser</div>
+				<div className="flex flex-col items-center">
+					<h2 className="text-center">Proposition de recettes à partir d'une liste d'ingrédients disponibles. Et autocomplétion des ingrédients durant la saisie. J'aimerai beaucoup pousser ce projet dans le futur !</h2>
 					<div className="flex flex-wrap justify-center mb-3">
 						{['Python', 'Trie', 'Data Cleaning'].map(
 							(keyword, index) => <div className="border rounded-lg m-1 p-1 whitespace-nowrap" key={index}>{keyword}</div>)}
