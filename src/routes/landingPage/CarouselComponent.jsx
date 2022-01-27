683140334baca6524b10cae1e7472227ac52b8d1
Charles <charlo22@hotmail.fr>
2022-01-27 14:25:21 +0100

    New carousel card + some styling

--- src/routes/landingPage/CarouselComponent.jsx
+++ src/routes/landingPage/CarouselComponent.jsx
@@ -1,6 +1,8 @@
 import Carousel from 'react-elastic-carousel';
 import { ReactComponent as CloudLogo } from './img/cloud-download-alt-solid.svg'
 import monte_carlo_pi from './img/monte-carlo-pi.png'
+import RL from './img/RL.png'
+import chili from './img/chili.jpg'
 
 const CarouselComponent = () => {
 
@@ -16,39 +18,53 @@ const CarouselComponent = () => {
 				<CloudLogo className="text-cdpurple w-2/4 pb-2" />
 				<div className="text-cdpurple text-2xl text-center">MusicSync</div>
 				<div className="flex flex-col items-center">
-					<h2 className="text-center">Téléchargement et synchronisation de playlist SoundCloud et YouTube.</h2>
+					<span className="text-center">Téléchargement et synchronisation de playlist SoundCloud et YouTube.</span>
 					<div className="flex flex-wrap justify-center mb-3">
 						{['Python', 'Crontab', 'Owncloud', 'Webdav', 'YoutubeDL'].map(
 							(keyword, index) => <div className="border rounded-lg m-1 p-1 whitespace-nowrap" key={index}>{keyword}</div>)}
 					</div>
-					<p><a href="https://django.charles.vin/musicsync" class="border border-cpurple p-2 m-2 rounded-md hover:bg-cpurple hover:text-white" target="_blank" rel="noopener noreferrer">Accéder à l'interface</a></p>
+					<a href="https://django.charles.vin/musicsync" class="border border-cpurple p-2 m-2 rounded-md hover:bg-cpurple hover:text-white" target="_blank" rel="noopener noreferrer">Accéder à l'interface</a>
 				</div>
 			</div>
 
 			<div className="bg-white flex-grow p-3 rounded-lg flex flex-col items-center">
-				<img src={monte_carlo_pi} alt="the web project" className="pb-2" />
+				<img src={RL} alt="The web project" className="pb-2" />
+				<div className="text-cdpurple text-2xl text-center">Apprentissage par renforcement</div>
+				<div className="flex flex-col items-center">
+					<span className="text-center">Compromis observation-action en apprentissage par renforcement</span>
+					<div className="flex flex-wrap justify-center mb-3">
+						{['RL', 'Tensorflow', 'POMDP'].map(
+							(keyword, index) => <div className="border rounded-lg m-1 p-1 whitespace-nowrap" key={index}>{keyword}</div>)}
+					</div>
+					<span class="border border-cpurple p-2 m-2 rounded-md hover:bg-cpurple hover:text-white">Voir l'article du blog</span>
+				</div>
+			</div>
+
+			<div className="bg-white flex-grow p-3 rounded-lg flex flex-col items-center">
+				<img src={monte_carlo_pi} alt="The web project" className="pb-2" />
 				<div className="text-cdpurple text-2xl text-center">Visualisation de la méthode Monte-Carlos pour estimer Pi</div>
 				<div className="flex flex-col items-center">
 					<div className="flex flex-wrap justify-center mb-3">
 						{['JavaScript', 'Html', 'Css', 'Mathématiques'].map(
 							(keyword, index) => <div className="border rounded-lg m-1 p-1 whitespace-nowrap" key={index}>{keyword}</div>)}
 					</div>
-					<p><a href="https://charlesattend.github.io/Monte-Carlo-pi-estimation-visualization/" class="border border-cpurple p-2 m-2 rounded-md hover:bg-cpurple hover:text-white" target="_blank" rel="noopener noreferrer">Accéder à l'interface</a></p>
+					<a href="https://charlesattend.github.io/Monte-Carlo-pi-estimation-visualization/" class="border border-cpurple p-2 m-2 rounded-md hover:bg-cpurple hover:text-white" target="_blank" rel="noopener noreferrer">Accéder à l'interface</a>
 				</div>
 			</div>
 
 			<div className="bg-white flex-grow p-3 rounded-lg flex flex-col items-center">
-				<img src={monte_carlo_pi} alt="image of the web project" className="pb-2" />
+				<img src={chili} alt="The web project" className="pb-2" />
 				<div className="text-cdpurple text-2xl text-center">Recipe Chooser</div>
 				<div className="flex flex-col items-center">
-					<h2 className="text-center">Proposition de recettes à partir d'une liste d'ingrédients disponibles. Et autocomplétion des ingrédients durant la saisie. J'aimerai beaucoup pousser ce projet dans le futur !</h2>
+					<span className="text-center">Proposition de recettes à partir d'une liste d'ingrédients disponibles. Et autocomplétion des ingrédients durant la saisie. J'aimerai beaucoup pousser ce projet dans le futur !</span>
 					<div className="flex flex-wrap justify-center mb-3">
 						{['Python', 'Trie', 'Data Cleaning'].map(
 							(keyword, index) => <div className="border rounded-lg m-1 p-1 whitespace-nowrap" key={index}>{keyword}</div>)}
 					</div>
-					<p><a href="https://github.com/CharlesAttend/Recipe-chooser" class="border p-2 m-2 rounded-md hover:bg-cpurple hover:text-white" target="_blank" rel="noopener noreferrer">Accéder au dépos GitHub</a></p>
+					<a href="https://github.com/CharlesAttend/Recipe-chooser" class="border p-2 m-2 rounded-md hover:bg-cpurple hover:text-white" target="_blank" rel="noopener noreferrer">Accéder au dépôt GitHub</a>
 				</div>
 			</div>
+
 		</Carousel>
 	)
 }
