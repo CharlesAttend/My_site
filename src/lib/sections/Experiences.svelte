d95937fc90b6a39f541637470e1cd652ee26595a
Charles Arch Linux <charles.vin@outlook.fr>
2023-09-07 23:07:03 +0200

    Expérience section

---
+++ src/lib/sections/Experiences.svelte
@@ -0,0 +1,31 @@
+<script>
+  import TimelineSection from "$lib/component/TimelineSection.svelte";
+  const timelines = [
+    {
+      title: "Oui oui oui",
+      date: "22 Aout",
+      text: "Veniam mollit sint elit consequat nulla proident ex magna id occaecat enim dolor nisi. Anim ex nisi consectetur reprehenderit ut consectetur anim proident veniam exercitation incididunt. Velit aliqua esse veniam ullamco eu proident non ipsum mollit ea sunt fugiat in ipsum. Proident consequat consectetur sunt adipisicing irure nisi tempor laboris laborum non sint voluptate cupidatat magna. Fugiat voluptate adipisicing reprehenderit duis eiusmod cupidatat.",
+      current: true,
+    },
+    {
+      title: "Oui oui oui",
+      date: "22 Aout",
+      text: "Elit occaecat culpa eiusmod occaecat sit. Esse sint mollit cupidatat laborum eu sint cupidatat laboris tempor. Ad enim ullamco consequat sunt incididunt commodo laboris sint duis amet.",
+    },
+    {
+      title: "Oui oui oui",
+      date: "22 Aout",
+      text: "Elit occaecat culpa eiusmod occaecat sit. Esse sint mollit cupidatat laborum eu sint cupidatat laboris tempor. Ad enim ullamco consequat sunt incididunt commodo laboris sint duis amet.",
+      current: true,
+    },
+  ];
+</script>
+
+<div class="">
+  <h2 class="text-center">Experiences</h2>
+  <ol class="m-2.5 relative border-l border-sapphire dark:border-gray-700">
+    {#each timelines as timeline}
+      <TimelineSection {...timeline} />
+    {/each}
+  </ol>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
   import LandingPage from "$lib/sections/LandingPage.svelte";
   import Me from "$lib/sections/Me.svelte";
   import Education from "$lib/sections/Education.svelte";
+  import Experiences from "$lib/sections/Experiences.svelte";
   export let data;
   console.log(data);
 </script>
@@ -10,4 +11,5 @@
   <LandingPage />
   <Me />
   <Education />
+  <Experiences />
 </div>
