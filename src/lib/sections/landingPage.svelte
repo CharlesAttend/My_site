7ba8a0c80f3e958c7cd52982fa29ea85ae425345
Charles Arch Linux <charles.vin@outlook.fr>
2023-09-07 22:32:07 +0200

    Using @layer tailwind directive

--- src/lib/sections/landingPage.svelte
+++ src/lib/sections/landingPage.svelte
@@ -1,15 +1,13 @@
 <div
-  class="p-2.5 bg-base h-screen w-screen flex-col justify-center items-center gap-2.5 inline-flex"
+  class="p-2.5 h-screen w-screen flex-col justify-center items-center gap-2.5 inline-flex"
 >
   <div class="justify-center items-center inline-flex">
     <div
       class="self-stretch p-2.5 shadow-xl border border-mauve border-sapphire flex-col justify-center items-center gap-2.5 inline-flex"
       id="animate-border"
     >
-      <div class="text-text text-4xl font-normal leading-10">Charles Vin</div>
-      <div class="text-text text-xs font-normal leading-none">
-        M2-DAC | Sorbonne université
-      </div>
+      <h1>Charles Vin</h1>
+      <p>M2-DAC | Sorbonne université</p>
     </div>
   </div>
 </div>
