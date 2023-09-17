f90a2b11480e5efd6c77d11b27c9a2f14545c728
Charles Arch Linux <charles.vin@outlook.fr>
2023-09-17 15:45:49 +0200

    commit to sync

---
+++ src/lib/sections/Hobbies.svelte
@@ -0,0 +1,26 @@
+<script>
+  import Card from "$lib/component/Card.svelte";
+</script>
+
+<h2 class="text-center">My main sports</h2>
+<div class="grid grid-cols-2 grid-rows-2 gap-5">
+  <div class="">
+    <Card title="Running" img={running}>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Mollitia
+      voluptas dolorum provident est voluptates sequi debitis eos, esse qui
+      fugit explicabo culpa voluptatem cupiditate incidunt ipsam nam enim vitae
+      impedit.
+    </Card>
+  </div>
+  <div>
+    <Card title="Climbing" img={climbing}>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Mollitia
+      voluptas dolorum provident est voluptates sequi debitis eos, esse qui
+      fugit explicabo culpa voluptatem cupiditate incidunt ipsam nam enim vitae
+      impedit.
+    </Card>
+  </div>
+  <div class="col-span-2">
+
+  </div>
+</div>
