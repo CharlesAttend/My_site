93dba964d84243098b9d0e534e88b846f84cb4e6
Charles <charles.vin@outlook.fr>
2023-09-17 19:44:09 +0200

    Particles !!!

--- src/lib/sections/LandingPage.svelte
+++ src/lib/sections/LandingPage.svelte
@@ -1,7 +1,45 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import { loadSlim } from "tsparticles-slim";
+  import particlesConfig from "$lib/assets/particlesConfig";
+
+  let ParticlesComponent;
+  onMount(async () => {
+    const module = await import("svelte-particles");
+
+    ParticlesComponent = module.default;
+  });
+
+  let onParticlesLoaded = (event) => {
+    const particlesContainer = event.detail.particles;
+
+    // you can use particlesContainer to call all the Container class
+    // (from the core library) methods like play, pause, refresh, start, stop
+  };
+
+  let particlesInit = async (engine) => {
+    // you can use main to customize the tsParticles instance adding presets or custom shapes
+    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+    // starting from v2 you can add only the features you need reducing the bundle size
+    //await loadFull(main);
+    await loadSlim(engine);
+  };
+</script>
+
+<svelte:component
+  this={ParticlesComponent}
+  id="tsparticles"
+  class="relative h-screen w-full bg-base"
+  style=""
+  options={particlesConfig}
+  on:particlesLoaded={onParticlesLoaded}
+  {particlesInit}
+/>
+
 <div
-  class="inline-flex h-screen w-full flex-col items-center justify-center gap-2.5 p-2.5"
+  class="absolute right-0 top-0 inline-flex h-screen w-full flex-col items-center justify-center gap-2.5 p-2.5"
 >
-  <div class="inline-flex items-center justify-center">
+  <div class="inline-flex items-center justify-center bg-base">
     <div
       class="inline-flex flex-col items-center justify-center gap-2.5 self-stretch border-2 border-mauve border-sapphire p-2.5 shadow-xl"
       id="animate-border"
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,13 @@ import { vitePreprocess } from "@sveltejs/kit/vite";
 
 export default defineConfig({
   preprocess: vitePreprocess(),
-
+  ssr: {
+    noExternal: [
+      "tsparticles",
+      "tsparticles-slim",
+      "tsparticles-engine",
+      "svelte-particles",
+    ], // add all tsparticles libraries here, they're not made for SSR, they're client only
+  },
   plugins: [sveltekit()],
 });
