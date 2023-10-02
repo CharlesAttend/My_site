d61c33a3f554d10d29a0d7b9ba88e94019d2de22
Charles Arch Linux <charles.vin@outlook.fr>
2023-10-02 18:41:30 +0200

    Removing everything for rewrite

--- .svelte-kit/output/server/nodes/0.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 0;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_layout.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/0.c077da64.js","_app/immutable/chunks/scheduler.e108d1fd.js","_app/immutable/chunks/index.0719bd3d.js"];
-export const stylesheets = ["_app/immutable/assets/0.1e5f7c8e.css"];
-export const fonts = [];
