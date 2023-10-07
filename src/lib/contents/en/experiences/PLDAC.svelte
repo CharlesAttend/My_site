6d640240669ae42ccaf54ab7efb76a7591e0f874
Charles Arch Linux <charles.vin@outlook.fr>
2023-10-07 16:50:26 +0200

    typo

--- src/lib/contents/en/experiences/PLDAC.svelte
+++ src/lib/contents/en/experiences/PLDAC.svelte
@@ -4,5 +4,5 @@
   </li>
   <li>Use of gradient-based approaches and the LIME and SHAP frameworks.</li>
   <li>Takeover, adaptation and documentation of the project.</li>
-  <li>Experience monitoring and evaluation of different models with WandB.</li>
+  <li>Experiences monitoring and evaluation of different models with WandB.</li>
 </ul>
