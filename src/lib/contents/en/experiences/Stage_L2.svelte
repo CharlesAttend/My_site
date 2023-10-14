ddda6000cac025225d202bb0fef47d1de9bca598
Charles <charles.vin@outlook.fr>
2023-10-14 22:14:32 +0200

    Fix ambiguous text

--- src/lib/contents/en/experiences/Stage_L2.svelte
+++ src/lib/contents/en/experiences/Stage_L2.svelte
@@ -1,6 +1,6 @@
 <ul>
   <li>
-    Assessed the reliability of a new model of eye-tracking glasses using
+    Assessed the reliability of a new model of eye-tracking glasses that were using
     machine learning.
   </li>
   <li>Wrote a user manual for these glasses.</li>
